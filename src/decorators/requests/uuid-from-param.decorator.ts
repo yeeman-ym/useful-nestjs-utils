a71d52052bff39f6e076b68b26ce37cc45b94025
ym <ym.yeeman@gmail.com>
2024-02-01 22:59:12 +0800

    feat: added requests decorators

---
+++ src/decorators/requests/uuid-from-param.decorator.ts
@@ -0,0 +1,18 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  Paramtype,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+
+export const UuidFromParam = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    const { id } = request.params;
+
+    return new ParseUUIDPipe().transform(id, {
+      type: 'param' as Paramtype,
+      data,
+    });
+  },
+);
